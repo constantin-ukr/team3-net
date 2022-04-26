03f2b9c31d2bd6bcc61ccad0760c4d2079493fc2
famasik <melnikmukola@gmail.com>
2022-04-27 01:05:15 +0300

    Update v.3

--- team3-net/CommentsService/Controllers/ItemsController.cs
+++ team3-net/CommentsService/Controllers/ItemsController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using CommentsService.Entities;
+using CommentsService.Repositories;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using static CommentsService.DTOs;
 
@@ -10,16 +12,22 @@ namespace CommentsService.Controllers
     public class ItemsController : ControllerBase
     {
         //DataBase here
-        private static readonly List<ItemDto> items = new()
-        {
-            new ItemDto(Guid.NewGuid(), "God of War", "Kratos and son are finally making their way to PC in a port of the much-loved action hack-and-slash.", DateTime.UtcNow),
-            new ItemDto(Guid.NewGuid(), "Dying Light 2", "The sequel to Techland's zombies and parkour action game is finally arriving in 2022 after multiple delays.", DateTime.UtcNow),
-            new ItemDto(Guid.NewGuid(), "Total War: Warhammer 3", "This is the final game in Creative Assembly's trilogy, bringing the last of the tabletop wargame's armies into digital form.", DateTime.UtcNow),
-        };
+
+        private readonly ItemRepository<Item> itemsRepository = new();
+
+
+        //private static readonly List<ItemDto> items = new()
+        //{
+        //    new ItemDto(Guid.NewGuid(), "God of War", "Kratos and son are finally making their way to PC in a port of the much-loved action hack-and-slash.", DateTime.UtcNow),
+        //    new ItemDto(Guid.NewGuid(), "Dying Light 2", "The sequel to Techland's zombies and parkour action game is finally arriving in 2022 after multiple delays.", DateTime.UtcNow),
+        //    new ItemDto(Guid.NewGuid(), "Total War: Warhammer 3", "This is the final game in Creative Assembly's trilogy, bringing the last of the tabletop wargame's armies into digital form.", DateTime.UtcNow),
+        //};
 
         [HttpGet]
-        public IEnumerable<ItemDto> Get()
+        public async Task<IEnumerable<ItemDto>> GetAsync()
         {
+            var items = (await itemsRepository.GetAllItemsAsync())
+                .Select(item => item.AsDto());
             return items;
         }
 
@@ -27,54 +35,61 @@ namespace CommentsService.Controllers
         //GET /items/{id}
         [HttpGet("{id}")]
 
-        public ItemDto GetById(Guid id)
+        public async Task<ActionResult<ItemDto>> GetByIdAsync(Guid id)
         {
 
-            var item = items.Where(item => item.id == id).SingleOrDefault();
+            var item = await itemsRepository.GetItemByIdAsync(id);
             if (item == null)
             {
-                return item;
+                return NotFound();
             }
-            return item;
+            return item.AsDto();
         }
         // POST /items/
         [HttpPost]
-        public ActionResult<ItemDto> Post(CreateItemDto createItemDto)
+        public async Task<ActionResult<ItemDto>> PostAsync(CreateItemDto createItemDto)
         {
-            var item = new ItemDto(Guid.NewGuid(), createItemDto.name, createItemDto.description, DateTime.UtcNow);
-            items.Add(item);
-            return CreatedAtAction(nameof(GetById), new { id = item.id }, item);
+            var item = new Item
+            {
+                Name = createItemDto.name,
+                Description = createItemDto.description
+            };
+
+            await itemsRepository.CreateAsync(item);
+            return CreatedAtAction(nameof(GetByIdAsync), new { id = item.Id }, item);
         }
 
         // PUT /items/{id}
         [HttpPut("{id}")]
-        public IActionResult Put(Guid id, UpdateItemDto updateItemDto)
+        public async Task<IActionResult> PutAsync(Guid id, UpdateItemDto updateItemDto)
         {
-            var existingItem = items.Where(item => item.id == id).SingleOrDefault();
-            
+            var existingItem = await itemsRepository.GetItemByIdAsync(id);
+
             if (existingItem == null)
             {
                 return NotFound();
             }
 
-            var updateItem = existingItem with
-            {
-                name = updateItemDto.name,
-                description = updateItemDto.description
-            };
+            existingItem.Name = updateItemDto.name;
+            existingItem.Description = updateItemDto.description;
 
-            var index = items.FindIndex(existingItem => existingItem.id == id);
-            items[index] = updateItem;
+            await itemsRepository.UpdateAsync(existingItem);
+
 
             return NoContent();
         }
-              
+
         // DELETE /items/{id}
         [HttpDelete("{id}")]
-        public IActionResult Delete(Guid id)
+        public async Task<IActionResult> DeleteAsync(Guid id)
         {
-            var index = items.FindIndex(existingItem => existingItem.id == id);
-            items.RemoveAt(index);
+            var item = await itemsRepository.GetItemByIdAsync(id);
+
+            if (item == null)
+            {
+                return NotFound();
+            }
+            await itemsRepository.DeleteAsync(item.Id);
             return NoContent();
         }
     }
---
+++ team3-net/CommentsService/Extensions.cs
@@ -0,0 +1,13 @@
+﻿using CommentsService.Entities;
+using static CommentsService.DTOs;
+
+namespace CommentsService
+{
+    public static class Extensions
+    {
+        public static ItemDto AsDto (this Item item)
+        {
+            return new ItemDto(item.Id, item.Name, item.Description, item.CreatedDate);
+        }
+    }
+}
