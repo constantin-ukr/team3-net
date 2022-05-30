57403f49dc5539baf069333f775df4d9fb8e86ef
rostyk-stakhiv <rostykstakhiv7@gmail.com>
2022-05-30 13:10:48 +0300

    Removed Web Shop

--- team3-net/eShopOnWeb/src/Web/Pages/Basket/CreditCardModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Microsoft.eShopWeb.Web.Pages.Basket;
-
-public class CreditCardModel
-{
-    public string CardNumber { get; set; }
-    public DateTime DateOfExpire { get; set; }
-    public int Cvc { get; set; }
-}
